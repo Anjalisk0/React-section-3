9013506ca8ad4e14348ba400ccfb4aebe0fdf622
Anjalisk0 <126849240+Anjalisk0@users.noreply.github.com>
2023-03-03 19:29:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './Components/Items';
+function App() {
+  return (
+    <>
+    <ExpenseItem />
+   <h2>lets get started!!
+
+   </h2>
+    </>
+            
+
+  );
+}
+export default App;
+
+
